e59c950a329fbbd45a2ccfd5b35f8a6696492df2
Gu Gyeongmin <rudals901@gmail.com>
2020-09-26 16:49:59 +0900

    modeling 1.4.7

--- modeling/wls_auto_arima.R
+++ modeling/wls_auto_arima.R
@@ -2,8 +2,103 @@ library(forecast)
 library(TTR)
 library(pracma)
 library(dplyr)
+library(tseries)
+setwd('C:/Users/user/Desktop/NOVUS_Futures/Preprocessing/data')
 
-setwd('F:/BigContest_NOVUS/NOVUS_Futures/Preprocessing/data')
+
+"""
+adf 검정
+"""
+# er_kt 차분1
+adf.test(kt_er,k = trunc((length(kt_er)-1)^(1/3)))
+diff_kter <- diff(kt_er)
+adf.test(diff_kter)
+
+# er_ss 차분1 
+adf.test(ss_er,k = trunc((length(kt_er)-1)^(1/3)))
+diff_sser <- diff(ss_er)
+adf.test(diff_sser)
+
+# wls_ht 차분1 
+adf.test(ht_wls,k = trunc((length(ht_wls)-1)^(1/3)))
+diff_sser <- diff(ht_wls)
+adf.test(diff_sser)
+
+"""
+er 
+kt 414
+ss 302
+
+wls 
+ht 212 
+kt 414
+
+"""
+pit_hh <- read.csv('./pit/pit_HH.csv')
+pit_ht <- read.csv('./pit/pit_HT.csv')
+pit_kt <- read.csv('./pit/pit_KT.csv')
+pit_lg <- read.csv('./pit/pit_LG.csv')
+pit_lt <- read.csv('./pit/pit_LT.csv')
+pit_nc <- read.csv('./pit/pit_NC.csv')
+pit_ob <- read.csv('./pit/pit_OB.csv')
+pit_sk <- read.csv('./pit/pit_SK.csv')
+pit_ss <- read.csv('./pit/pit_SS.csv')
+pit_wo <- read.csv('./pit/pit_WO.csv')
+head(pit_wo)
+
+hit_hh <- read.csv('./hit/hit_HH.csv')
+hit_ht <- read.csv('./hit/hit_HT.csv')
+hit_kt <- read.csv('./hit/hit_KT.csv')
+hit_lg <- read.csv('./hit/hit_LG.csv')
+hit_lt <- read.csv('./hit/hit_LT.csv')
+hit_nc <- read.csv('./hit/hit_NC.csv')
+hit_ob <- read.csv('./hit/hit_OB.csv')
+hit_sk <- read.csv('./hit/hit_SK.csv')
+hit_ss <- read.csv('./hit/hit_SS.csv')
+hit_wo <- read.csv('./hit/hit_WO.csv')
+head(hit_wo)
+
+
+hh_wls <- pit_hh[,4]
+ht_wls <- pit_ht[,4]
+kt_wls <- pit_kt[,4]
+lg_wls <- pit_lg[,4]
+lt_wls <- pit_lt[,4]
+nc_wls <- pit_nc[,4]
+ob_wls <- pit_ob[,4]
+sk_wls <- pit_sk[,4]
+ss_wls <- pit_ss[,4]
+wo_wls <- pit_wo[,4]
+
+hh_er <- pit_hh[,3]
+ht_er <- pit_ht[,3]
+kt_er <- pit_kt[,3]
+lg_er <- pit_lg[,3]
+lt_er <- pit_lt[,3]
+nc_er <- pit_nc[,3]
+ob_er <- pit_ob[,3]
+sk_er <- pit_sk[,3]
+ss_er <- pit_ss[,3]
+wo_er <- pit_wo[,3]
+
+hh_ba <- hit_hh[,3]
+ht_ba <- hit_ht[,3]
+kt_ba <- hit_kt[,3]
+lg_ba <- hit_lg[,3]
+lt_ba <- hit_lt[,3]
+nc_ba <- hit_nc[,3]
+ob_ba <- hit_ob[,3]
+sk_ba <- hit_sk[,3]
+ss_ba <- hit_ss[,3]
+wo_ba <- hit_wo[,3]
+head(wo_ba)
+
+for(i in 0:4){
+  for(j in 0:4){
+    for(k in 0:1)cat(arima(kt_wls,c(i,k,j))$aic,i,k,j,"\n")
+  }
+}
+
 
 """
 --승률 ARIMA--
@@ -20,67 +115,45 @@ SK 0,1,1
 키움 0,1,1
 """
 
-
-
 # 한화 3,0,2
-pit_hh <- read.csv('./pit/pit_HH.csv')
 head(pit_hh)
-hh_wls <- pit_hh[,4]
 head(hh_wls)
 auto.arima(hh_wls)
 head(hh_wls)
 
 # 기아 0,1,0  
-pit_ht <- read.csv('./pit/pit_HT.csv')
-ht_wls <- pit_ht[,4]
 head(ht_wls)
 auto.arima(ht_wls)
 
 # 크트 0,1,0 
-pit_kt <- read.csv('./pit/pit_KT.csv')
-kt_wls <- pit_kt[,4]
 head(kt_wls)
 auto.arima(kt_wls)
 
 # 엘지 2,0,0 
-pit_lg <- read.csv('./pit/pit_LG.csv')
-lg_wls <- pit_lg[,4]
 head(lg_wls)
 auto.arima(lg_wls)
 
 # 롯데 1,0,3
-pit_lt <- read.csv('./pit/pit_LT.csv')
-lt_wls <- pit_lt[,4]
 head(lt_wls)
 auto.arima(lt_wls)
 
 # 엔씨 0,1,0
-pit_nc <- read.csv('./pit/pit_NC.csv')
-nc_wls <- pit_nc[,4]
 head(nc_wls)
 auto.arima(nc_wls)
 
 # 두산 4,1,1 
-pit_ob <- read.csv('./pit/pit_OB.csv')
-ob_wls <- pit_ob[,4]
 head(ob_wls)
 auto.arima(ob_wls)
 
 # 스크 0,1,0 
-pit_sk <- read.csv('./pit/pit_SK.csv')
-sk_wls <- pit_sk[,4]
 head(sk_wls)
 auto.arima(sk_wls)
 
 # 샘숭 0,1,0 
-pit_ss <- read.csv('./pit/pit_SS.csv')
-ss_wls <- pit_ss[,4]
 head(ss_wls)
 auto.arima(ss_wls)
 
 # 키움 1,0,0 
-pit_wo <- read.csv('./pit/pit_WO.csv')
-wo_wls <- pit_wo[,4]
 head(wo_wls)
 auto.arima(wo_wls)
 
@@ -147,6 +220,73 @@ head(ss_er)
 auto.arima(ss_er)
 
 # 키움 0,1,1
-wo_er <- pit_wo[,5]
+wo_er <- pit_wo[,3]
 head(wo_er)
 auto.arima(wo_er)
+
+"""
+-- non ma 자책점 ARIMA --
+  한화 1,2,1
+기아 1,2,1
+kt 0,1,0
+lg 1,1,0
+롯데 1,0,0
+nc 0,2,1
+두산 2,1,1
+sk 1,0,1 
+삼성 1,0,0 
+키움 0,1,1
+
+"""
+
+# 한화 2,0,1//// 000
+hh_er <- pit_hh[,3]
+hh_er
+auto.arima(hh_er)
+
+# 기아 1,1,2//// 112
+ht_er <- pit_ht[,3]
+head(pit_ht)
+auto.arima(ht_er)
+
+# kt 0,1,0//// 011
+kt_er <- pit_kt[,3]
+head(kt_er)
+auto.arima(kt_er)
+
+# lg 1,0,2//// 000
+lg_er <- pit_lg[,3]
+head(lg_er)
+auto.arima(lg_er)
+
+# 롯데 1,0,0//// 000
+lt_er <- pit_lt[,3]
+head(lt_er)
+auto.arima(lt_er)
+
+# nc 2,1,0//// 000
+nc_er <- pit_nc[,3]
+head(nc_er)
+auto.arima(nc_er)
+
+# 두산 2,1,1//// 000
+ob_er <- pit_ob[,3]
+head(ob_er)
+auto.arima(ob_er)
+
+# sk 0,1,1//// 100
+sk_er <- pit_sk[,3]
+head(sk_er)
+auto.arima(sk_er)
+
+# 삼성 1,0,0//// 001 
+ss_er <- pit_ss[,3]
+head(ss_er)
+auto.arima(ss_er)
+
+# 키움 0,1,1//// 202
+wo_er <- pit_wo[,3]
+head(wo_er)
+auto.arima(wo_er)
+
+
